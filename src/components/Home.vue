d0cb252fd54d92f6512e8b9e85ba2179e6eef563
Alex <kleshnin.alexander@gmail.com>
2019-01-08 23:23:36 +0200

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <v-layout text-xs-center wrap>
+      <v-flex mb-4>
+        <vuetify-google-autocomplete
+          id="map"
+          label="Search Address"
+          v-on:placechanged="getAddressData"
+        ></vuetify-google-autocomplete>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  data() {
+    return {
+      address: null
+    };
+  },
+  methods: {
+    getAddressData: function(addressData, placeResultData) {
+      this.address = addressData;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+import 'vuetify/src/stylus/app.styl'
+import VuetifyGoogleAutocomplete from 'vuetify-google-autocomplete'
+
 import App from './App.vue'
+
+Vue.use(Vuetify, {
+  iconfont: 'md',
+})
+
+Vue.use(VuetifyGoogleAutocomplete, {
+  apiKey: 'AIzaSyDyJUwEpWPLTDZrX9TVeq5m8vGQScqyZCA'
+});
 
 Vue.config.productionTip = false
 
